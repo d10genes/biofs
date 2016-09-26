9d8abb8eb72de661b145c9c10c525d37f3c5af84
d10genes <wcbeard10@gmail.com>
2016-09-26 13:32:43 -0400

    writing random cycle generator

--- biofs.fsx
+++ biofs.fsx
@@ -24,21 +24,68 @@ let inp = """0 -> 3
 9 -> 6
 """
 
-let pEulCycLine = int .>> pstring " -> " .>>. (sepBy int (pchar ','))
+type UserState = unit // doesn't have to be unit, of course
+type Parser<'t> = Parser<'t, UserState>
+
+let str s = pstring s
+let sepList : Parser<_> = between (str "[") (str "]") (sepBy pint32 (str ";"))
+
+let (p: Parser<int32 list, 'a> ) = sepBy int (pchar ',')
+let pEulCycLine : Parser<_> = int .>> pstring " -> " .>>. (sepBy int (pchar ','))
 // run pEulCycLine "14 -> 3"  // Success: (14, [3])
 // run pEulCycLine "14 -> 30,23"  // Success: (14, [30; 23])
 let pEulCyc = sepEndBy1 pEulCycLine nl
+run pEulCyc inp
 
-let tupsL2eL xs =
+type Edge<'a,'b> = ('a * 'b)
+type EdgeList<'a,'b> = Edge<'a,'b> list
+type EdgeSet<'a, 'b  when 'a: comparison and 'b: comparison> = Set<Edge<'a,'b>>
+type EdgeMap<'a, 'b  when 'a: comparison and 'b: comparison> = Map<'a, Set<Edge<'a,'b>>>
+// type EdgeMap<'a,'b when 'a : comparison> = Map<'a, Set<'a,'b>>
+
+let (Success((rawvals), _, _)) = run (pEulCyc) inp
+let (Success((vals), _, _)) = run (pEulCyc |>> tupsL2eL) inp
+
+//   ('a * 'b) Map
+let m = Map.empty.Add(1, [(1, 2)])
+
+// : EdgeList<'a,'b>
+let tupsL2eL (xs: ('a * 'b list) list) : EdgeList<'a,'b> =
     let rec tupL2El = function
         | h, (y :: ys) -> (h, y) :: tupL2El (h, ys)
         | h, [] -> []
-    List.concat (List.map tupL2El xs)
+    List.collect tupL2El xs
 
-let (Success((vals), _, _)) = run (pEulCyc |>> tupsL2eL) inp
+let tupsL2eM (xs: ('a * 'b list) list): EdgeMap<'a, 'b> =
+    let tupL2Em (h, ys) =
+        (h, (Set.map (fun x -> (h, x)) (Set.ofList ys)))
+    xs |> List.map tupL2Em |> Map.ofList
+
+let remove (emap: EdgeMap<'a, 'b>) (start, fin): EdgeMap<'a, 'b> =
+    let edges = emap.Item start
+
+    Map.add start (edges.Remove (start, fin)) emap
+
+let res = tupsL2eM rawvals
+let res2 = remove res (0, 3)
+
+let unexploredEdges = Set.ofList vals
+// let selectNode =
+let unitePairs xs = Set.union (Set.map fst xs) (Set.map snd xs)
+let allNodes = unitePairs unexploredEdges
+let startNode = List.ofSeq  allNodes |> List.min
+
+let chooseNode f edgeSet =
+    let allNodes = unitePairs edgeSet
+    List.ofSeq  allNodes |> f
+
+chooseNode List.min unexploredEdges
+
+let randCycle f unexploredEdges =
+    let startNode = chooseNode f unexploredEdges
 
 
-
+randCycle List.min unexploredEdges
 
 readWrite "data/ch3/dataset_200_7.txt" strandsD (solveDbg)
 
